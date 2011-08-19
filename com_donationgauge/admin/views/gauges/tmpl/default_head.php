c51bdee0d7000c371c3eb701c1a4b5cf577a02cd
Jonah Dahlquist <jonah@nucleussystems.com>
2011-08-19 16:09:58 -0700

    Made table headers into ordering links

--- com_donationgauge/admin/views/gauges/tmpl/default_head.php
+++ com_donationgauge/admin/views/gauges/tmpl/default_head.php
@@ -1,24 +1,27 @@
 <?php
 defined('_JEXEC') or die; // no direct access allowed
+
+$listOrder = $this->escape($this->state->get('list.ordering'));
+$listDirection = $this->escape($this->state->get('list.direction'));
 ?>
 <tr>
 	<th width="5">
-		<?php echo JText::_('COM_DONATIONGAUGE_ADMIN_GAUGES_HEADING_ID'); ?>
+		<?php echo JHtml::_('grid.sort', 'COM_DONATIONGAUGE_ADMIN_GAUGES_HEADING_ID', 'a.id', $listDirection, $listOrder); ?>
 	</th>
 	<th width="20">
-		<input type="checkbox" name="toggle" value="" onclick="checkAll(<?php echo count($this->items); ?>);" />
+		<input type="checkbox" name="toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this);" />
 	</th>
 	<th>
-		<?php echo JText::_('COM_DONATIONGAUGE_ADMIN_GAUGES_HEADING_TITLE'); ?>
+		<?php echo JHtml::_('grid.sort', 'JGLOBAL_TITLE', 'a.title', $listDirection, $listOrder); ?>
 	</th>
 	<th width="90">
-		<?php echo JText::_('COM_DONATIONGAUGE_ADMIN_GAUGES_HEADING_PROGRESS'); ?>
+		<?php echo JHtml::_('grid.sort', 'COM_DONATIONGAUGE_ADMIN_GAUGES_HEADING_PROGRESS', 'a.progress', $listDirection, $listOrder); ?>
 	</th>
 	<th width="90">
-		<?php echo JText::_('COM_DONATIONGAUGE_ADMIN_GAUGES_HEADING_TOTAL'); ?>
+		<?php echo JHtml::_('grid.sort', 'COM_DONATIONGAUGE_ADMIN_GAUGES_HEADING_TOTAL', 'a.total', $listDirection, $listOrder); ?>
 	</th>
 	<th width="80">
-		<?php echo JText::_('JSTATUS'); ?>
+		<?php echo JHtml::_('grid.sort', 'JSTATUS', 'a.status', $listDirection, $listOrder); ?>
 	</th>
 	
 </tr>

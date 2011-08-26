6afbded4dca0bc203e55c0dd546f73a27afacfb3
Jonah Dahlquist <jonah@nucleussystems.com>
2011-08-26 16:09:27 -0700

    Improved edit page toolbar

--- com_donationgauge/admin/views/gauge/view.html.php
+++ com_donationgauge/admin/views/gauge/view.html.php
@@ -6,31 +6,44 @@ jimport('joomla.application.component.view');
 class DonationGaugeViewGauge extends JView
 {
 
+	protected $form;
+	protected $item;
+	protected $state;
+
 	public function display($tmpl=null)
 	{
-		$form = $this->get('Form');
-		$item = $this->get('Item');
+		$this->form = $this->get('Form');
+		$this->item = $this->get('Item');
 		
 		if (count($errors = $this->get('Errors'))) {
 			JError::raiseError(500, implode('<br />', $errors));
 			return false;
 		}
 		
-		$this->form = $form;
-		$this->item = $item;
-		
 		$this->addToolbar();
-		
+
+		JRequest::setVar('hidemainmenu', true);	
 		parent::display($tmpl);
 	}
 	
 	public function addToolBar()
 	{
-		JRequest::setVar('hidemainmenu', true);
 		$isNew = $this->item->id == 0;
+		$user = JFactory::getUser();
+		$userId = $user->get('id');
+		
 		JToolBarHelper::title($isNew ? JText::_('COM_DONATIONGAUGE_ADMIN_GAUGE_NEW') : JText::_('COM_DONATIONGAUGE_ADMIN_GAUGE_EDIT'));
+		
+		JToolBarHelper::apply('gauge.apply');
 		JToolBarHelper::save('gauge.save');
+		JToolBarHelper::save2new('gauge.save2new');
+		
+		if (!$isNew) {
+			JToolBarHelper::save2copy('gauge.save2copy');
+		}
+		
 		JToolBarHelper::cancel('gauge.cancel', $isNew ? 'JTOOLBAR_CANCEL' : 'JTOOLBAR_CLOSE');
+
 	}
 
 }
